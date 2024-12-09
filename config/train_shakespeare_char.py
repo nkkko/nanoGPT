8319771604fd4a4e3fab2fa90cdaad5a8c9fca75
nkkko <niko@daytona.io>
2024-12-09 11:18:02 +0000

    wandb

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -15,27 +15,27 @@ wandb_project = 'shakespeare-char'
 wandb_run_name = 'mini-gpt'
 
 dataset = 'shakespeare_char'
-gradient_accumulation_steps = 1
-batch_size = 256
+gradient_accumulation_steps = 2
+batch_size = 64
 block_size = 256 # context of up to 256 previous characters
 
 # Dataloader optimizations
-num_workers = 8
+num_workers = 4
 pin_memory = True
 
 # baby GPT model :)
 n_layer = 6
 n_head = 6
 n_embd = 384
-dropout = 0.2
+dropout = 0.3
 
-learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+learning_rate = 5e-4  # Slightly lower initial learning rate
+warmup_iters = 200    # Increase warmup period
+
+max_iters = 4000
+lr_decay_iters = 4000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
-
-warmup_iters = 100 # not super necessary potentially
 
 # on macbook also add
 # device = 'cpu'  # run on cpu only
